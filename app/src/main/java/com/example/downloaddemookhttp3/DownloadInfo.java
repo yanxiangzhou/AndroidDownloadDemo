3c2ab8fa8dea91c1cdc1f4a25167b76aea97a3d1
zyxscuec <15337121992@163.com>
2023-04-22 22:05:03 +0800

    修改变量名

--- app/src/main/java/com/example/downloaddemookhttp3/DownloadInfo.java
+++ app/src/main/java/com/example/downloaddemookhttp3/DownloadInfo.java
@@ -2,45 +2,45 @@ package com.example.downloaddemookhttp3;
 
 public class DownloadInfo {
 
-    private final String fileName;
-    private final String filePath;
-    private final String url;
+    private final String mFileName;
+    private final String mmFilePath;
+    private final String mUrl;
 
     public DownloadInfo(Builder builder) {
-        this.fileName = builder.fileName;
-        this.filePath = builder.filePath;
-        this.url = builder.url;
+        this.mFileName = builder.mFileName;
+        this.mmFilePath = builder.mFilePath;
+        this.mUrl = builder.mUrl;
     }
 
     public String getUlr() {
-        return this.url;
+        return mUrl;
     }
 
     public String getFileName() {
-        return fileName;
+        return mFileName;
     }
 
     public String getFilePath() {
-        return filePath;
+        return mmFilePath;
     }
 
     public static class Builder {
-        private String fileName;
-        private String filePath;
-        private String url;
+        private String mFileName;
+        private String mFilePath;
+        private String mUrl;
 
         public Builder setFileName(String fileName) {
-            this.fileName = fileName;
+            this.mFileName = fileName;
             return this;
         }
 
         public Builder setUrl(String url) {
-            this.url = url;
+            this.mUrl = url;
             return this;
         }
 
         public Builder setFilePath(String filePath) {
-            this.filePath = filePath;
+            this.mFilePath = filePath;
             return this;
         }
 
